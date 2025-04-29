c5630e47d3adbe524c0c4e7c2d85611511b6aa7f
dainavipurav <dainavipurav@gmail.com>
2025-04-29 17:38:36 +0530

    feat : Learning custom middleware

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,9 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+function logger(req, res, next) {
+  console.log(req.method);
+  console.log(req.url);
+  next();
+}
 
 app.use(logger);
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
